b59bc8d80412079fcfd79e9d5ed475e775170ca0
NextChai <75498301+NextChai@users.noreply.github.com>
2021-08-01 17:30:29 -0400

    Add user class, subclass ClientUser from it

--- tixte/user.py
+++ tixte/user.py
@@ -1,3 +1,23 @@
+"""
+The MIT License (MIT)
+Copyright (c) 2015-present Rapptz
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
 import datetime
 from typing import (
     Any, 
@@ -7,14 +27,55 @@ from typing import (
     ClassVar
 )
 
-from .errors import NotImplementedError
-
 __all__ = (
+    'User',
     'ClientUser',
-    'User'
 )
 
-class ClientUser:
+class User:
+    """
+    The base User class.
+    
+    Attributes
+    ----------
+    id: :class:`str`
+        The ID of the clientuser.
+    username: :class:`str`
+        The username of the clientuser.
+    pro: :class:`bool`
+        Whether or not the user has pro.
+    beta: :class:`bool`
+        Whether or not the user is in the beta.
+    admin: :class:`bool`
+        Whether or not the user is an admin.
+    staff: :class:`bool`
+        Whether or not the user is staff.
+    avatar: Optional[:class:`str`]
+        The user's avatar, if any.
+    """
+    
+    __slots__ = ('_status', '_raw', 'id', 'username', 'avatar', 'pro', 'beta', 'admin', 'staff')
+    
+    def __init__(
+        self,
+        status: Any,
+        data: Dict[str, Union[str, bool]]
+    ) -> None:
+        self._status = status
+        self._raw = data
+        
+        self.id: str = data['id']
+        self.username: str = data['username']
+        self.avatar: Optional[str] = data['avatar']
+        self.pro: bool = data['pro']
+        self.beta: bool = data['beta']
+        self.admin: bool = data['admin']
+        self.staff: bool = data['staff']
+        
+    def __str__(self) -> str:
+        return self.username
+
+class ClientUser(User):
     """
     The Clent's User profile. Because the client acts on behalf of your upload key, this fetches
     YOUR information.
@@ -61,22 +122,17 @@ class ClientUser:
         status: Any,
         data: Dict[str, Union[str, bool]]
     ) -> None:
-        self._raw: ClassVar[Dict] = data
-        self._status: Any = status
+        super().__init__(status, data)
         
-        self.id: str = data['id']
-        self.username: str = data['username']
         self.mfa_enabled: bool = data['mfa_enabled']
-        self.pro: bool = data['pro']
-        self.beta: bool = data['beta']
-        self.admin: bool = data['admin']
-        self.staff: bool = data['staff']
         self.email: str = data['email']
         self.email_verified: bool = data['email_verified']
         self.phone: Optional[Any] = data['phone']
-        self.avatar: Optional[str] = data['avatar']
         self.upload_region: str = data['upload_region']
         self.raw_last_login = data['last_login']
+        
+        self._raw: ClassVar[Dict] = data
+        self._status: Any = status
     
     @property
     def last_login(self) -> datetime.datetime:
@@ -84,17 +140,12 @@ class ClientUser:
 
     def _dump_attrs_to_client(self, client):
         ignored = ('_raw', '_status')
+        total = {}
         for slot in self.__slots__:
             if slot in ignored:
                 continue
             value = getattr(self, slot)
             setattr(client, slot, value)
-    
-class User:
-    def __init__(
-        self,
-        status: Any,
-        data: Dict[str, Union[str, bool]]
-    ) -> None:
-        self._status = status
-        self._raw = data
+            total[slot] = value
+        setattr(client, '_raw_user', total)
+        
