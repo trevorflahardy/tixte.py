629b150be025c21ee49503bfd1720cacea9a16f2
NextChai <75498301+NextChai@users.noreply.github.com>
2022-04-27 14:23:49 -0400

    Fix some setup.py fixes

--- setup.py
+++ setup.py
@@ -1,40 +1,45 @@
+import re
 import pathlib
 from setuptools import setup
 
-HERE = pathlib.Path(__file__).parent
+here = pathlib.Path(__file__).parent
+readme = (here / "README.md").read_text()
 
-README = (HERE / "README.md").read_text()
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+  
+version = ''
+with open('tixte/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
-VERSION = "0.0.6"
-
-requirements = ['aiohttp>=3.6.0,<3.8.0']
 
 setup(
-    author="Iced Chai",
-    name="tixte",
-    description="The async wrapper for the Tixte API.",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/NextChai/Tixte",
+    author='NextChai',
+    name='tixte',
+    description='The async wrapper for the Tixte API.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/NextChai/Tixte',
     project_urls={
         'Report a Bug': 'https://github.com/NextChai/Tixte/issues',
         "Contribute": 'https://github.com/NextChai/Tixte/pulls',
     },
-    version=VERSION,
-    license="MIT",
+    version=version,
+    license='MIT',
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    packages=["tixte"],
+    packages=[
+        'tixte'
+    ],
     include_package_data=True,
     install_requires=requirements,
     entry_points={
         "console_scripts": [
             "square=square.__main__:main",
         ]
-    },
+    },  
 )
