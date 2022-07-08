3ca30dae07f90f7292f9beb60b7ef9a21592d84e
NextChai <75498301+NextChai@users.noreply.github.com>
2022-07-08 13:49:41 -0400

    Black formatting

--- docs/extensions/details.py
+++ docs/extensions/details.py
@@ -3,24 +3,31 @@ from docutils.parsers.rst import states, directives
 from docutils.parsers.rst.roles import set_classes
 from docutils import nodes
 
+
 class details(nodes.General, nodes.Element):
     pass
+
 
 class summary(nodes.General, nodes.Element):
     pass
 
+
 def visit_details_node(self, node):
     self.body.append(self.starttag(node, 'details', CLASS=node.attributes.get('class', '')))
+
 
 def visit_summary_node(self, node):
     self.body.append(self.starttag(node, 'summary', CLASS=node.attributes.get('summary-class', '')))
     self.body.append(node.rawsource)
 
+
 def depart_details_node(self, node):
     self.body.append('</details>\n')
 
+
 def depart_summary_node(self, node):
     self.body.append('</summary>')
+
 
 class DetailsDirective(Directive):
     final_argument_whitespace = True
@@ -42,14 +49,17 @@ class DetailsDirective(Directive):
 
         if self.arguments:
             summary_node = summary(self.arguments[0], **self.options)
-            summary_node.source, summary_node.line = self.state_machine.get_source_and_line(self.lineno)
+            (
+                summary_node.source,
+                summary_node.line,
+            ) = self.state_machine.get_source_and_line(self.lineno)
             node += summary_node
 
         self.state.nested_parse(self.content, self.content_offset, node)
         return [node]
 
+
 def setup(app):
     app.add_node(details, html=(visit_details_node, depart_details_node))
     app.add_node(summary, html=(visit_summary_node, depart_summary_node))
     app.add_directive('details', DetailsDirective)
-
