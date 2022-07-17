3ca32d57d6247cf4d0578bd00839fd66ba5cc5ee
NextChai <75498301+NextChai@users.noreply.github.com>
2022-07-17 01:07:23 -0400

    Start adding enums

---
+++ tixte/enums.py
@@ -0,0 +1,40 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2021-present NextChai
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+from __future__ import annotations
+
+import enum
+
+
+class Region(enum.Enum):
+    us_east_one = 'us-east-1'
+
+
+class Premium(enum.Enum):
+    free = 0
+    turbo = 1
+    turbo_charged = 2
+
+
+class UploadPermissions(enum.Enum):
+    viewer = 1
+    manager = 2
+    owner = 3
