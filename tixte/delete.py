21539b23d70c80b7e75e129a9f3746fe2c5ec06d
NextChai <75498301+NextChai@users.noreply.github.com>
2022-08-07 02:44:54 -0400

    Implement a delete response

---
+++ tixte/delete.py
@@ -0,0 +1,53 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2021-present NextChai
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, Tuple, Any
+
+from .abc import Object
+
+if TYPE_CHECKING:
+    from .state import State
+
+__all__: Tuple[str, ...] = ('DeleteResponse',)
+
+
+class DeleteResponse(Object):
+    """Represents the response from Tixte when deleting a file.
+
+    Attributes
+    ----------
+    message: :class:`str`
+        The message from Tixte with the status of the deletion.
+    extra: Dict[:class:`str`, :class:`str`]
+        Any extra metadata that Tixte passed along with the deletion.
+    """
+
+    __slots__: Tuple[str, ...] = ('_state', 'message', 'extra')
+
+    def __init__(self, *, state: State, data: Dict[Any, Any]) -> None:
+        self._state: State = state
+        self.message: str = data.pop('message')
+        self.extra: Dict[str, Any] = data
+
+    def __repr__(self) -> str:
+        return '<DeleteResponse message={0.message!r}>'.format(self)
