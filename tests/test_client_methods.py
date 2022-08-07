9473513a92490e73b375770e84786c1c6fb7da92
NextChai <75498301+NextChai@users.noreply.github.com>
2022-08-07 02:42:30 -0400

    Add base for pytests

--- tests/test_client_methods.py
+++ tests/test_client_methods.py
@@ -1,3 +1,4 @@
+import aiohttp
 import pytest
 import os
 
@@ -21,94 +22,18 @@ except KeyError as exc:
 TIXTE_ACCOUNT_ID: str = '89b1c121b4f24b22aa8607748981c34a'
 TIXTE_TESTING_URL: str = 'https://imgur.com/a/TKpgn1w'
 TIXTE_TESTING_DOMAIN: str = 'tixte-testing.tixte.co'
-
+TIXTE_OTHER_USER: str = '71dc9871ca28446c8baffbd74430f2ad'
+TIXTE_TESTING_UPLOAD_NAME: str = 'xqc.png'
+TIXTE_TESTING_UPLOAD_ID: str = 'l5r277bx20a'
 
 @pytest.mark.asyncio
-async def test_client_fetch_client_user() -> None:
-    client = tixte.Client(TIXTE_MASTER_KEY, TIXTE_MASTER_DOMAIN)
-    async with client:
-        user = await client.fetch_client_user()
+async def test_fetch_user() -> None:
+    session = aiohttp.ClientSession()
+    client = tixte.Client(TIXTE_MASTER_KEY, TIXTE_MASTER_DOMAIN, session=session)
+    
+    user = await client.fetch_user(TIXTE_ACCOUNT_ID)
+    
+    await client.cleanup()
 
     assert user.id == TIXTE_ACCOUNT_ID
     assert user.username == 'TTesting'
-    assert user.email == TIXTE_ACCOUNT_EMAIL
-    assert user.email_verified == True
-
-    assert client.user == user
-
-
-@pytest.mark.asyncio
-async def test_client_fetch_user() -> None:
-    client = tixte.Client(TIXTE_MASTER_KEY, TIXTE_MASTER_DOMAIN)
-    async with client:
-        user = await client.fetch_user(TIXTE_ACCOUNT_ID)
-
-    assert user.id == TIXTE_ACCOUNT_ID
-    assert user.username == 'TTesting'
-
-    assert user in client.users
-
-    cached_user = client.get_user(TIXTE_ACCOUNT_ID)
-    assert cached_user is not None
-    assert cached_user == user
-
-
-@pytest.mark.asyncio
-async def test_client_url_to_file() -> None:
-    client = tixte.Client(TIXTE_MASTER_KEY, TIXTE_MASTER_DOMAIN)
-    async with client:
-        file = await client.url_to_file(TIXTE_TESTING_URL, filename='testing.png')
-
-    assert file.filename == 'testing.png'
-
-
-@pytest.mark.asyncio
-async def test_client_file_methods() -> None:
-    client = tixte.Client(TIXTE_MASTER_KEY, TIXTE_MASTER_DOMAIN)
-    async with client:
-        file = await client.url_to_file(TIXTE_TESTING_URL, filename='testing.png')
-        upload = await client.upload(file)
-
-        upload = await upload.to_file()
-        assert upload.filename == 'testing.png'
-
-        assert upload.filename == 'testing'
-        assert upload.extension == '.png'
-
-        # Now we can delete this upload
-        response = await upload.delete()
-        print(response.message)
-
-
-@pytest.mark.asyncio
-async def test_client_fetch_domains() -> None:
-    client = tixte.Client(TIXTE_MASTER_KEY, TIXTE_MASTER_DOMAIN)
-    async with client:
-        domains = await client.fetch_domains()
-
-        testing_domain: tixte.Domain = next(
-            domain for domain in domains if domain.url == 'testing.tixte.io'
-        )
-
-        assert testing_domain.url == 'testing.tixte.io'
-        assert testing_domain.owner_id == TIXTE_ACCOUNT_ID
-
-        domain_owner = await testing_domain.fetch_owner()
-        assert domain_owner == await client.fetch_client_user()
-
-    assert client.domains == domains
-
-
-@pytest.mark.asyncio
-async def test_client_configuration() -> None:
-    client = tixte.Client(TIXTE_MASTER_KEY, TIXTE_MASTER_DOMAIN)
-    async with client:
-        config = await client.fetch_config()
-
-    assert config.to_dict() == {
-        'title': None,
-        'description': None,
-        'author': {'name': None, 'url': None},
-        'color': 5793266,
-        'provider': {'name': None, 'url': None},
-    }
